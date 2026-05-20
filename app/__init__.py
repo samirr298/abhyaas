0fa76be8c423b058522487430208c89863532e52
sijansubedi30-lgtm <sijansubedi30@gmail.com>
2026-05-20 12:51:51 +0545

    we setup the database for register  and added some required files

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 
 from app.routes.auth_route import AuthRoutes
 from datetime import timedelta
-
+from app.database import Database
 
 def create_app():
     app = Flask(__name__)
     # Secret key required for session/flash functionality. Replace in production.
+    Database()
     app.secret_key = 'change-me-in-production'
     # keep session cookies persistent when requested (remember me)
     app.permanent_session_lifetime = timedelta(days=30)
